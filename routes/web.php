4f826512f50e4461f9395c3639688ff5242f4f8c
”KeisukeShishido” <aya.kotori0723@gmail.com>
2019-12-01 09:43:38 +0000

    ルーティング追加

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 
     Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit' );
 });
